68b85bbb2a26ed37130ce367aa1dec8a25a18ad1
Noelia <52795935+noecoro@users.noreply.github.com>
2024-06-12 01:27:19 -0300

    un try catch

--- Test/Test.cs
+++ Test/Test.cs
@@ -56,54 +56,11 @@ namespace Test
                 pudo = l + m1;
 
             }
-            //catch (TipoIncorrectoException ex)
-            //{
-            //    Console.WriteLine(ex.ToString());
-            //}
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error inesperado: {ex.Message}");
-            }
-
-            Console.WriteLine("\nPrimer intento agregar libro en escaner de mapas");
-            try
-            {
-                bool pudo = m + l1;
-            }
             catch (TipoIncorrectoException ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-
-            Console.WriteLine("\nSegundo intento agregar mapa en escaner de libros");
-            try
-            {
-                bool pudo = l + m1;
-            }
-            catch (TipoIncorrectoException ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            try
-            {
-                bool pudo = l == m1;
-               
-            }
-            catch (TipoIncorrectoException ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            try
-            {
-                bool pudo = m == l1;
-              
-            }
-            catch (TipoIncorrectoException ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
+            
 
             l1.AvanzarEstado();
             l1.AvanzarEstado();
